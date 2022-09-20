4384d2d49eaa897debe1824540eac0c53eda6071
Leviathan-bit <84622086+Leviathan-bit@users.noreply.github.com>
2022-09-20 10:15:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ apt update
 apt install nala -y
 nala upgrade -y
 nala install sudo wget curl zsh apt-transport-https software-properties-common gpg lsb-release ca-certificates -y
-nala install git libnm-dev network-manager-gnome build-essential meson ninja-build cmake pkg-config wayland-protocols libwayland-dev libdrm-dev libxkbcommon-dev libudev-dev libseat-dev xwayland libcairo-dev libpango1.0-dev libpangocairo-1.0-0 libegl-dev libinput-dev libxcb-xkb-dev libgles-dev xdg-desktop-portal-wlr libwlroots-dev libglib2.0-dev libxfce4ui-2-dev libpolkit-agent-1-dev -y
+nala install git build-essential meson ninja-build cmake pkg-config wayland-protocols libwayland-dev libdrm-dev libxkbcommon-dev libudev-dev libseat-dev xwayland libcairo-dev libpango1.0-dev libpangocairo-1.0-0 libegl-dev libinput-dev libxcb-xkb-dev libgles-dev xdg-desktop-portal-wlr libwlroots-dev libglib2.0-dev libxfce4ui-2-dev libpolkit-agent-1-dev -y
 
 # Install Hyprland
 mkdir Git
@@ -51,7 +51,7 @@ cp -aR microsoft.gpg /etc/apt/trusted.gpg.d/
 #install aditional pacakge
 nala upgrade -y
 nala install waybar swaylock swaybg swayidle grim wl-clipboard rofi xwayland dunst xdg-desktop-portal-wlr qtwayland5 libnm-dev network-manager-gnome sddm slurp -y
-nala install nemo brave-browser code geany kitty viewnior vlc ranger file-roller nemo-fileroller lxappearance mpd mpc ncmpcpp python3-pip pavucontrol -y
+nala install nemo libnm-dev network-manager-gnome brave-browser code geany kitty viewnior vlc ranger file-roller nemo-fileroller lxappearance mpd mpc ncmpcpp python3-pip pavucontrol -y
 nala install composer network-manager libnss3-tools jq xsel php8.1-cli php8.1-curl php8.1-mbstring php8.1-mcrypt php8.1-xml php8.1-zip php8.1-sqlite3 php8.1-mysql php8.1-pgsql php8.1-fpm -y
 
 # install Themes and icons
