7dd4ad7905a1fc199e9833412736f64c403912db
Leviathan-bit <kadalgurun521@gmail.com>
2022-09-20 18:08:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,27 @@ nala upgrade -y
 nala install sudo wget curl zsh apt-transport-https software-properties-common gpg lsb-release ca-certificates -y
 nala install btop git build-essential seatd meson ninja-build cmake pkg-config wayland-protocols -y
 nala install xwayland libcairo-dev wlr-randr libwayland-dev libdrm-dev libxkbcommon-dev libudev-dev libseat-dev -y 
-nala install libpango1.0-dev libpangocairo-1.0-0 libegl-dev libinput-dev libxcb-xkb-dev libgles-dev -y
+nala install libpango1.0-dev libpangocairo-1.0-0 libegl-dev libinput-dev libxcb-xkb-dev libgles-dev wireplumber -y
 nala install xdg-desktop-portal-wlr libwlroots-dev libglib2.0-dev libxfce4ui-2-dev libpolkit-agent-1-dev -y
+
+# Add aditional repo
+# Add Brave repo
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Add PHP Surya repo
+echo "deb https://packages.sury.org/php/ bullseye main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+
+# Add Visual Studio Code repo
+cp -aR vscode.list /etc/apt/sources.list.d/
+cp -aR microsoft.gpg /etc/apt/trusted.gpg.d/
+
+#install aditional pacakge
+nala upgrade -y
+nala install waybar sway swaylock swaybg swayidle grim wl-clipboard rofi xwayland dunst xdg-desktop-portal-wlr qtwayland5 libnm-dev network-manager-gnome sddm slurp -y
+nala install nemo libnm-dev network-manager-gnome brave-browser code geany kitty viewnior vlc ranger file-roller nemo-fileroller lxappearance mpd mpc ncmpcpp python3-pip pavucontrol -y
+nala install composer network-manager libnss3-tools jq xsel php8.1-cli php8.1-curl php8.1-mbstring php8.1-mcrypt php8.1-xml php8.1-zip php8.1-sqlite3 php8.1-mysql php8.1-pgsql php8.1-fpm -y
 
 # Install Hyprland
 mkdir Git
@@ -37,25 +56,6 @@ ninja install
 cd ..
 cd ..
 
-# Add aditional repo
-# Add Brave repo
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-# Add PHP Surya repo
-echo "deb https://packages.sury.org/php/ bullseye main" | sudo tee /etc/apt/sources.list.d/sury-php.list
-wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
-
-# Add Visual Studio Code repo
-cp -aR vscode.list /etc/apt/sources.list.d/
-cp -aR microsoft.gpg /etc/apt/trusted.gpg.d/
-
-#install aditional pacakge
-nala upgrade -y
-nala install waybar swaylock swaybg swayidle grim wl-clipboard rofi xwayland dunst xdg-desktop-portal-wlr qtwayland5 libnm-dev network-manager-gnome sddm slurp -y
-nala install nemo libnm-dev network-manager-gnome brave-browser code geany kitty viewnior vlc ranger file-roller nemo-fileroller lxappearance mpd mpc ncmpcpp python3-pip pavucontrol -y
-nala install composer network-manager libnss3-tools jq xsel php8.1-cli php8.1-curl php8.1-mbstring php8.1-mcrypt php8.1-xml php8.1-zip php8.1-sqlite3 php8.1-mysql php8.1-pgsql php8.1-fpm -y
-
 # install Themes and icons
 nala install papirus-icon-theme -y
 git clone https://github.com/EliverLara/Nordic.git /usr/share/themes/Nordic
@@ -68,6 +68,9 @@ fc-cache -vf
 echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
 curl 'https://liquorix.net/add-liquorix-repo.sh' | sudo bash
 
+
+# Purge some package
+nala purge foot tilix sway -y
 sudo usermod -aG sudo $USER
 nala upgrade -y
 
